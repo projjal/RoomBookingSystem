181ca5fbd062571326f9ffa081f65b5ada22ee4e
Vidushi Vashishth <vvashish@adobe.com>
2018-07-23 17:33:05 +0530

    updating utils

--- react/src/utils/index.js
+++ react/src/utils/index.js
@@ -59,29 +59,3 @@ class TableView extends Component{
 }
 
 export default TableView;
-
-
-// class App extends Component{
-//   constructor(props){
-//     super(props);
-//     this.state={
-//       headings:["ID", "NAME", "SALARY"],
-//       players:[
-//       {id:101, "name":'sachin', 'salary':1000},
-//       {id:102, "name":'saurav', 'salary':2000},
-//       {id:103, "name":'rahul', 'salary':1500},
-//       {id:104, "name":'sehwag', 'salary':3000},
-//       {id:105, "name":'virat', 'salary':8000}
-//     ]
-//     }
-//   }
-//   render(){
-//     return(
-//       <div>
-//       <TableView heading={this.state.headings} data={this.state.players}/>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
