30b918d8b2cacd7502fcf130d19faa28d993f7ed
pchanda <pchanda@adobe.com>
2018-07-25 17:25:43 +0530

    removed extra booking system

--- bookingsystem/src/main/java/com/adobe/theforce/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.adobe.theforce;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- bookingsystem/src/main/java/com/adobe/theforce/web/LoginController.java
+++
@@ -1,6 +0,0 @@
-package com.adobe.theforce.web;
-
-//@RestController
-public class LoginController {
-
-}
